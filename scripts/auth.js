795ae8435f65b1b5e1d8141f3c4842b2197525e2
reanzi <nziku99@gmail.com>
2019-01-16 14:44:30 +0300

    signing in a user, done

--- scripts/auth.js
+++ scripts/auth.js
@@ -15,6 +15,7 @@ signupForm.addEventListener("submit", e => {
       console.log(cred.user);
       //   setTimeout(() => {}, 2000);
 
+      //close the modal and reset form
       const modal = document.querySelector("#modal-signup");
       M.Modal.getInstance(modal).close();
       signupForm.reset();
@@ -30,5 +31,24 @@ logout.addEventListener("click", e => {
   e.preventDefault();
   auth.signOut().then(() => {
     console.log("User has Signed Out");
+  });
+});
+
+// login
+const loginForm = document.querySelector("#login-form");
+loginForm.addEventListener("submit", e => {
+  e.preventDefault();
+
+  // get User info
+  const email = loginForm["login-email"].value;
+  const password = loginForm["login-password"].value;
+
+  auth.signInWithEmailAndPassword(email, password).then(cred => {
+    console.log(cred.user.email + " is loged in");
+
+    //close the modal and reset form
+    const modal = document.querySelector("#modal-login");
+    M.Modal.getInstance(modal).close();
+    loginForm.reset();
   });
 });
